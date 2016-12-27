6b01b1248cec1a73c38b409266c52e22ae2a90c2
Torrey Jones <torreypjones@gmail.com>
2016-12-26 20:21:46 -0700

    made everything output to output/current

--- run_discovery.bash
+++ run_discovery.bash
@@ -20,4 +20,6 @@ mkdir output 2>&1 > /dev/null || echo "output directory already exists"
 
 myDate=$(date +%Y-%j)
 
-nmap -vvvvv -o -PE -PP -sS -sV -T5 $NETWORKS --webxml -oA output/$myDate 2>&1 | tee -a output/$myDate.log
+mkdir output/$myDate 2>&1 > /dev/null || echo "output directory already exists"
+mv -f output/current* output/$myDate
+nmap -vvvvv -o -PE -PP -sS -sV -T5 $NETWORKS --webxml -oA output/current 2>&1 | tee -a output/current.log
